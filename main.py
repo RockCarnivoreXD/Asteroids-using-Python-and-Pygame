bfbdf6fef6d7f9ae954133dfe956a6ed98100a2f
RockCarnivoreXD <rockcarnivorexd@gmail.com>
2025-09-13 12:44:58 -0600

    Game Loop working

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
         "\nScreen width: 1280"
         "\nScreen height: 720"
         )
-
+    while True:
+      for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+      screen.fill((0, 0, 0))
+      pygame.display.flip()
 
 if __name__ == "__main__":
     main()
