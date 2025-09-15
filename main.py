28172a658ea8a3f70fff8b1a0ac8239b6eee3922
RockCarnivoreXD <rockcarnivorexd@gmail.com>
2025-09-15 12:09:02 -0600

    FPS implemented

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
+    new_clock = pygame.time.Clock()
+    dt = 0
     print(
         "Starting Asteroids!"
         "\nScreen width: 1280"
         "\nScreen height: 720"
         )
+    
     while True:
       for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
       screen.fill((0, 0, 0))
       pygame.display.flip()
+      
+      dt = new_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
